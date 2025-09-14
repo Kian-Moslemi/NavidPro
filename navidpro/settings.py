cf050ea31c02faa78f70c7c09dda2d5aad6e95ab
Kian-Moslemi <kian.moslemi@gmail.com>
2025-09-14 15:02:56 +0330

    Update settings.py

--- navidpro/settings.py
+++ navidpro/settings.py
@@ -80,6 +80,7 @@ WSGI_APPLICATION = 'navidpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
@@ -87,15 +88,15 @@ DATABASES = {
         conn_max_age=600
     )
 }
+'''
 
 
-#
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
